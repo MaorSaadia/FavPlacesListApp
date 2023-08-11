42476e33c15331b3cbc0338d49beb2c3de6e2f5c
Maor <maorsa9@gmail.com>
2023-08-11 11:18:26 +0300

    Adding DataBase using Sqlite to save the places

---
+++ util/database.js
@@ -0,0 +1,113 @@
+import * as SQLite from 'expo-sqlite';
+import { Place } from '../models/place';
+
+const database = SQLite.openDatabase('places.db');
+
+export function init() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
+        id INTEGER PRIMARY KEY NOT NULL,
+        title TEXT NOT NULL,
+        imageUri TEXT NOT NULL,
+        address TEXT NUT NULL,
+        lat REAL NOT NULL,
+        lng REAL NOT NULL
+    )`,
+        [],
+        () => {
+          resolve();
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
+
+export function insertPlace(place) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.imageUri,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          //console.log(result);
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
+
+export function fetchPlaces() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM places',
+        [],
+        (_, result) => {
+          const places = [];
+          for (const dp of result.rows._array)
+            places.push(
+              new Place(
+                dp.title,
+                dp.imageUri,
+                {
+                  address: dp.address,
+                  lat: dp.lat,
+                  lng: dp.lng,
+                },
+                dp.id
+              )
+            );
+
+          resolve(places);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
+
+export function dropTable() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `DROP TABLE IF EXISTS places`,
+        [],
+        () => {
+          resolve();
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
+
+// dropTable()
+//   .then(() => {
+//     console.log('Table dropped successfully.');
+//   })
+//   .catch((error) => {
+//     console.error('Error dropping table:', error);
+//   });
